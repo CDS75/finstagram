01b86969589884918fc302cd679c47ac1969d6af
Chris <christinedsavard@me.com>
2021-11-11 01:54:38 +0000

    W1D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,32 @@
+
+def humanized_time_ago(minute_num)
+  if minute_num >= 60
+    "#{minute_num / 60} hours ago"
+  else
+    "#{minute_num} minutes ago"
+  end
+end
+
 get '/' do
+  username = "sharky_j"
+  avatar_url = "http://naserca.com/images/sharky_j.jpg"
+  photo_url = "http://naserca.com/images/shark.jpg"
+  time_ago_in_minutes = 15
+  like_count = 0
+  comment_count = 1
+  comments = [
+    "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
+  ]
+
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutes ago"
+  end
+end
+
+ 
+ get '/' do
     File.read(File.join('app/views', 'index.html'))
   end
-  
+
